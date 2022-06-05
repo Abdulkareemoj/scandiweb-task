3e6f362f04236244a5eee7b095de34cf9218d5a3
Abdulkareemoj <abdulkareemojerinde@gmail.com>
2022-06-05 20:19:53 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./Styles.scss";
 import React from "react";
 import Productlist from "./Components/Productlist";
 import Navbar from "./Components/Navbar";
-import LinkList from './LinkList';
+
 
 
 export default function App() {
