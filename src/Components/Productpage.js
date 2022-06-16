fd8c047ec5aa653845e8d0ac7fd10e529f5a1285
Abdulkareemoj <abdulkareemojerinde@gmail.com>
2022-06-16 18:47:38 +0100

    fix productpage

--- src/Components/Productpage.js
+++ src/Components/Productpage.js
@@ -11,22 +11,40 @@ class Productpage extends React.Component {
 	render() {
 		const {productImage,img1,img2,img3,price, productName} =  this.props
 		return (
-			<div className="page-container">
+			<div className="page-container  container containerpall">
 			
 			<div className="product-thumbnail">
-				<img src= {img1} className="product-image" alt={`${productName}`} />
-				<img src= {img2} className="product-image" alt={`${productName}`} />
-				<img src= {img3} className="product-image" alt={`${productName}`}/>
+				<img src= {img1} className="product-thumbnail" alt={`${productName}`} />
+				<img src= {img2} className="product-thumbnail"alt={`${productName}`} />
+				<img src= {img3} className="product-thumbnail" alt={`${productName}`}/>
 			</div>
 			
-			<div className="product-card">
-					<img src= {productImage} className="product-image" alt={`${productName}`} />
+			<div className="product-image">
+					<img src= {productImage} alt={`${productName}`} />
 				
 				</div> 
+
 			   <div className="product-details">
+
+			   <strong>  <p className="product-name"> {productName}</p></strong>
 			   <p className="product-name">{productName}</p>
-					<p className="product-price"> {price}</p>
-			   </div> </div>
+			   <h2>
+				SIZE
+			   </h2> 	 
+			   <input type="radio" value="XS" name="size" /> XS
+        <input type="radio" value="S" name="size" /> S
+        <input type="radio" value="M" name="size" /> M
+		<input type="radio" value="L" name="size" /> L
+
+<h2>COLOR</h2>
+<input type="radio" value="Male" name="color1" /> Male
+        <input type="radio" value="color2" name="size" /> Female
+        <input type="radio" value="color3" name="size" /> Other
+
+<h2>PRICE</h2>
+					<p className="product-price">{price}</p>
+					<input type="radio" value="ADD TO CART" name="ADD" /> ADD TO CART
+								   </div> </div>
 		)};
 		
 	} 
