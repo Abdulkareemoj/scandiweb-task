60d304541b2d66ad8be36ab3d33a37cf7b0f3d92
Abdulkareemoj <abdulkareemojerinde@gmail.com>
2022-06-24 10:06:31 +0100

    add state functionality to productpage

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -18,46 +18,121 @@ const DATA = [
 //   let [sizeValue, setSizeValue] = useState("");
 //   console.log(sizeValue);
 
-// constructor() {
-//     super();
+
 //     this.state = {
 //       name: "React"
 //     };
 //     this.onChangeValue = this.onChangeValue.bind(this);
-//   }
+//   
 
  
 
 class Productpage extends React.Component {
+    state = {
+		value: "XS",
+		value2: "color1"
+	};
+	onChange= e=> {
+		this.setState({value :e.target.value});
+	}
 	render() {
 		const { productImage, img1, img2, img3, price, productName,productItem,productDesc } = this.props;
 		return (
-			 
-				<div className="product-details">
-					<strong>
-						{" "}
-						<p className="product-name"> {productName}</p>
-					</strong>
-					<p className="product-item">{productItem}</p>
-					<h2 className="size-choice">SIZE</h2>
-	 
+            <div className="product-details">
+            <strong>
+                {" "}
+                <p className="product-name"> {productName}</p>
+            </strong>
+            <p className="product-item">{productItem}</p>
+
+            <h2 className="size-choice">SIZE</h2>
+
+            {/* <div onChange={this.onChangeValue}>  */}
+            <label>XS 
+                <input 
+                className="size-style" 
+                type="radio" 
+                value="XS" 
+                checked={value === "XS"}
+                onChange={this.onChange} 
+                /> 
+                </label>
+            <label>S
+                <input 
+                className="size-style" 
+                type="radio" 
+                value="S" 
+                checked={value === "S"}
+                onChange={this.onChange} 
+                />
+                </label>
+            <label>M
+                <input 
+                className="size-style" 
+                type="radio" 
+                value="M" 
+                checked={value === "M"}
+                onChange={this.onChange} 
+                />
+            </label>
+            <label>L
+                <input 
+                className="size-style"
+                type="radio"
+                value="L" 
+                checked={value === "L"}
+                onChange={this.onChange} 
+                /> 
+                </label>
+
+
+           
+					{/* <div onChange={this.onChangeValue}>  */}
 					<input className="size-style" type="radio" value="XS" name="size" /> XS
 					<input className="size-style"  type="radio" value="S" name="size" /> S
 					<input className="size-style" type="radio" value="M" name="size" /> M
 					<input className="size-style" type="radio" value="L" name="size" /> L
-				 
-						<h2 className="color-choice">COLOR</h2>
-					<input type="radio" value="Male" name="color1" /> Male
-					<input type="radio" value="color2" name="size" /> Female
-					<input type="radio" value="color3" name="size" /> Other
-					<h2 className="price-choice">PRICE</h2>
-					<p className="product-price">{price}</p>
-					<input className="cta-utto" type="button" value="ADD TO CART" name="ADD" /> 
-                    </div>{" "}
-<div>
-			<p className="product-desc">{productDesc}</p>
-			</div>
+				
+
+<h2 className="color-choice">COLOR</h2>
+            <label>color1
+                <input 
+                className="color-style" 
+                type="radio" 
+                value="color1" 
+                checked={value2 === "color1"}
+                onChange={this.onChange} 
+                />
+                </label>
+
+                <label>color2
+                <input 
+                className="color-style" 
+                type="radio" 
+                value="color2" 
+                checked={value2 === "color2"}
+                onChange={this.onChange} 
+                />
+                </label>
+
+                <label>color3
+                <input 
+                className="color-style" 
+                type="radio" 
+                value="color3" 
+                checked={value2 === "color3"}
+                onChange={this.onChange} 
+                />
+                </label>
+
+            <h2 className="price-choice">PRICE</h2>
+            <p className="product-price">{price}</p>
+
+            <input className="cta-button" type="button" value="ADD TO CART" name="ADD" /> 
+            </div>
+      
 		);
 	}
 }
+
 export default Productpage;
--- src/Components/Productpage.js
+++ src/Components/Productpage.js
@@ -7,7 +7,7 @@ const DATA = [
 		img3: "link3",
 	},
 ];
- 
+
 
 // const productSize = ["S", "M", "X", "XXL"];
 //   //map
@@ -18,100 +18,140 @@ const DATA = [
 //   let [sizeValue, setSizeValue] = useState("");
 //   console.log(sizeValue);
 
-// constructor() {
-//     super();
-//     this.state = {
-//       name: "React"
-//     };
-//     this.onChangeValue = this.onChangeValue.bind(this);
-//   }
 
- 
+//     
+
 
 class Productpage extends React.Component {
-	render() {
-		const { productImage, img1, img2, img3, price, productName,productItem,productDesc } = this.props;
-		return (
-			<div className="page-container  container containerpall">
-				<div className="product-thumbnail">
-					<img
-						src={img1}
-						className="product-thumbnail"
-						alt={`${productName}`}
-					/>
-					<img
-						src={img2}
-						className="product-thumbnail"
-						alt={`${productName}`}
-					/>
-					<img
-						src={img3}
-						className="product-thumbnail"
-						alt={`${productName}`}
-					/>
-				</div>
-				<div className="product-image">
-					<img src={productImage} alt={`${productName}`} />
-				</div>
-				<div className="product-details">
-					<strong>
-						{" "}
-						<p className="product-name"> {productName}</p>
-					</strong>
-					<p className="product-item">{productItem}</p>
-					<h2 className="size-choice">SIZE</h2>
-					{/* <div onChange={this.onChangeValue}>  */}
-					<input className="size-style" type="radio" value="XS" name="size" /> XS
-					<input className="size-style"  type="radio" value="S" name="size" /> S
-					<input className="size-style" type="radio" value="M" name="size" /> M
-					<input className="size-style" type="radio" value="L" name="size" /> L
-					{/* </div> */}
-
-					{/* <div className="main max-w-lg mx-auto">
-      <div className="grid grid-cols-4 gap-8 gap-y-4">
-        {sizeData?.map(({ id, value }) => (
-          <div className="-mt-2" key={id}>
-            <input
-              id={id}
-              className="invisible radio_custom"
-              type="radio"
-              value={value}
-              checked={sizeValue === { sizeValue }}
-              onChange={(e) => {
-                const nodes = e.target.parentElement.parentElement.childNodes;
-                for (let i = 0; i < nodes.length; i++) {
-                  nodes[i].lastChild.firstChild.classList.remove("show_border");
-                }
-                e.target.nextSibling.firstChild.classList.toggle("show_border");
-                return setSizeValue(e.target.value);
-              }}
-            />
-            <label htmlFor={id} className="radio_custom_label">
-              <div
-                className={`border border-gray-300 py-3 text-center cursor-pointer "
-                        }`}
-              >
-                {value}
-              </div>
-            </label>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}; */}
-
-
-						<h2 className="color-choice">COLOR</h2>
-					<input type="radio" value="Male" name="color1" /> Male
-					<input type="radio" value="color2" name="size" /> Female
-					<input type="radio" value="color3" name="size" /> Other
-					<h2 className="price-choice">PRICE</h2>
-					<p className="product-price">{price}</p>
-					<input className="cta-utto" type="button" value="ADD TO CART" name="ADD" /> </div>{" "}
-			<p className="product-desc">{productDesc}</p>
-			</div>
-		);
+	state = {
+		value: "XS",
+		value2: "color1"
+	};
+	onChange= e=> {
+		this.setState({[e.target.name] : e.target.value});
 	}
-}
+		//     this.onChangeValue = this.onChangeValue.bind(this);
+		//   }
+
+render() {
+	const { value, value2 } = this.state;
+	const { productImage, img1, img2, img3, price, productName, productItem, productDesc } = this.props;
+	return (
+		<div className="page-container  container containerpall">
+			<div className="product-thumbnail">
+				<img
+					src={img1}
+					className="product-thumbnail"
+					alt={`${productName}`}
+				/>
+				<img
+					src={img2}
+					className="product-thumbnail"
+					alt={`${productName}`}
+				/>
+				<img
+					src={img3}
+					className="product-thumbnail"
+					alt={`${productName}`}
+				/>
+			</div>
+			<div className="product-image">
+				<img src={productImage} alt={`${productName}`} />
+			</div>
+			<div className="product-details">
+				<strong>
+					{" "}
+					<p className="product-name"> {productName}</p>
+				</strong>
+				<p className="product-item">{productItem}</p>
+
+				<h2 className="size-choice">SIZE</h2>
+
+				{/* <div onChange={this.onChangeValue}>  */}
+				<label>XS 
+					<input 
+					className="size-style" 
+					type="radio" 
+					value="XS" 
+					name="value"
+					checked={value === "XS"}
+					onChange={this.onChange} 
+					/> 
+					</label>
+				<label>S
+					<input 
+					className="size-style" 
+					type="radio" 
+					value="S" 
+					name="value"
+					checked={value === "S"}
+					onChange={this.onChange} 
+					/>
+					</label>
+				<label>M
+					<input 
+					className="size-style" 
+					type="radio" 
+					value="M" 
+					name="value"
+					checked={value === "M"}
+					onChange={this.onChange} 
+					/>
+				</label>
+				<label>L
+					<input 
+					className="size-style"
+					type="radio"
+					value="L" 
+					name="value"
+					checked={value === "L"}
+					onChange={this.onChange} 
+					/> 
+					</label>
+
+
+				<h2 className="color-choice">COLOR</h2>
+				<label>color1
+					<input 
+					className="color-style" 
+					type="radio" 
+					value="color1" 
+					name="value2"
+					checked={value2 === "color1"}
+					onChange={this.onChange} 
+					/>
+					</label>
+
+					<label>color2
+					<input 
+					className="color-style" 
+					type="radio" 
+					value="color2" 
+					name="value2"
+					checked={value2 === "color2"}
+					onChange={this.onChange} 
+					/>
+					</label>
+
+					<label>color3
+					<input 
+					className="color-style" 
+					type="radio" 
+					value="color3" 
+					name="value2"
+					checked={value2 === "color3"}
+					onChange={this.onChange} 
+					/>
+					</label>
+
+				<h2 className="price-choice">PRICE</h2>
+				<p className="product-price">{price}</p>
+
+				<input className="cta-button" type="button" value="ADD TO CART" name="ADD" />
+				 </div>{" "}
+			<p className="product-desc">{productDesc}</p>
+		</div>
+	);
+				}
+			}
 export default Productpage;
