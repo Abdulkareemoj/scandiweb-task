362260b5999dca280b4f17a2a8f2c298b607bfce
Abdulkareemoj <abdulkareemojerinde@gmail.com>
2022-06-25 21:59:23 +0100

    reset

--- src/Components/Productpage.js
+++ src/Components/Productpage.js
@@ -27,20 +27,9 @@ const DATA = [
 	// 	colors: "color1"
 	// };
 
-<<<<<<< HEAD
 	// onChange= e=> {
 	// 	this.setState({[e.target.name] : e.target.value});
 	// }
-=======
-class Productpage extends React.Component {
-	state = {
-		value: "XS",
-		value2: "color1"
-	};
-	onChange= e=> {
-		this.setState({[e.target.name] : e.target.value});
-	}
->>>>>>> parent of 3e9b2ef (fixed productpage)
 		//     this.onChangeValue = this.onChangeValue.bind(this);
 		//   }
 
@@ -81,9 +70,10 @@ class Productpage extends React.Component {
 				</strong>
 				<p className="product-item">{productItem}</p>
 
+
+
 				<h2 className="size-choice">SIZE</h2>
 
-<<<<<<< HEAD
 	<div className="sizebutton-style">
 	<input
           onChange={(e) => setSizes(e.target.value)}
@@ -176,85 +166,6 @@ class Productpage extends React.Component {
         </label>
       </div>
     
-=======
-				{/* <div onChange={this.onChangeValue}>  */}
-				<label>XS 
-					<input 
-					className="size-style" 
-					type="radio" 
-					value="XS" 
-					name="value"
-					checked={value === "XS"}
-					onChange={this.onChange} 
-					/> 
-					</label>
-				<label>S
-					<input 
-					className="size-style" 
-					type="radio" 
-					value="S" 
-					name="value"
-					checked={value === "S"}
-					onChange={this.onChange} 
-					/>
-					</label>
-				<label>M
-					<input 
-					className="size-style" 
-					type="radio" 
-					value="M" 
-					name="value"
-					checked={value === "M"}
-					onChange={this.onChange} 
-					/>
-				</label>
-				<label>L
-					<input 
-					className="size-style"
-					type="radio"
-					value="L" 
-					name="value"
-					checked={value === "L"}
-					onChange={this.onChange} 
-					/> 
-					</label>
-
-
-				<h2 className="color-choice">COLOR</h2>
-				<label>color1
-					<input 
-					className="color-style" 
-					type="radio" 
-					value="color1" 
-					name="value2"
-					checked={value2 === "color1"}
-					onChange={this.onChange} 
-					/>
-					</label>
-
-					<label>color2
-					<input 
-					className="color-style" 
-					type="radio" 
-					value="color2" 
-					name="value2"
-					checked={value2 === "color2"}
-					onChange={this.onChange} 
-					/>
-					</label>
-
-					<label>color3
-					<input 
-					className="color-style" 
-					type="radio" 
-					value="color3" 
-					name="value2"
-					checked={value2 === "color3"}
-					onChange={this.onChange} 
-					/>
-					</label>
-
->>>>>>> parent of 3e9b2ef (fixed productpage)
 				<h2 className="price-choice">PRICE</h2>
 				<p className="product-price">{price}</p>
 
