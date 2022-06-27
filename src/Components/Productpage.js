2dac3d4603b6c661e38fcb3c0d10810e959f3dd2
Abdulkareemoj <abdulkareemojerinde@gmail.com>
2022-06-27 22:03:34 +0100

    fix

--- src/Components/Productpage.js
+++ src/Components/Productpage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 const DATA = [
 	{
@@ -33,16 +33,25 @@ const DATA = [
 		//     this.onChangeValue = this.onChangeValue.bind(this);
 		//   }
 
-		export default function() {
-			const [colors, setColors] = useState("color 1");
-			const [sizes, setSizes] = useState("small");
+		
 // render() {
 	// const [colors, setColors] = useState("color 1");
 	// const [sizes, setSizes] = useState("small");
   
 	const { productImage, img1, img2, img3, price, productName, productItem, productDesc } = this.props;
-	return (
-		<div className="page-container  container containerpall">
+	
+
+class Productpage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      colors: "color 1",
+      sizes: "small"
+    };
+  }
+  render() {
+    return (
+      <div className="page-container  container containerpall">
 			<div className="product-thumbnail">
 				<img
 					src={img1}
@@ -71,108 +80,119 @@ const DATA = [
 				<p className="product-item">{productItem}</p>
 
 
+      <div>
+        <h2 className="size-choice">SIZE</h2>
+        <div className="sizebutton-style">
+          <input
+            onChange={(e) => this.setState({ sizes: e.target.value })}
+            className="hidden_radio"
+            type="radio"
+            id="small"
+            name="colors"
+            value="small"
+          />
+          <label
+            className={`${
+              this.state.sizes === "small" ? "button-selected" : "button"
+            }`}
+            for="small"
+          >
+            small
+          </label>
+          <input
+            onChange={(e) => this.setState({ sizes: e.target.value })}
+            className="hidden_radio"
+            type="radio"
+            id="medium"
+            name="colors"
+            value="medium"
+          />
+          <label
+            className={`${
+              this.state.sizes === "medium" ? "button-selected" : "button"
+            }`}
+            for="medium"
+          >
+            medium
+          </label>
+          <input
+            onChange={(e) => this.setState({ sizes: e.target.value })}
+            className="hidden_radio"
+            type="radio"
+            id="large"
+            name="colors"
+            value="large"
+          />
+          <label
+            className={`${
+              this.state.sizes === "large" ? "button-selected" : "button"
+            }`}
+            for="large"
+          >
+            large
+          </label>
+          <input
+            onChange={(e) => this.setState({ sizes: e.target.value })}
+            className="hidden_radio"
+            type="radio"
+            id="xlarge"
+            name="colors"
+            value="xlarge"
+          />
+          <label
+            className={`${
+              this.state.sizes === "xlarge" ? "button-selected" : "button"
+            }`}
+            for="xlarge"
+          >
+            xlarge
+          </label>
+        </div>
 
-				<h2 className="size-choice">SIZE</h2>
+        <h2 className="color-choice">COLOR</h2>
 
-	<div className="sizebutton-style">
-	<input
-          onChange={(e) => setSizes(e.target.value)}
-          className="hidden_radio"
-          type="radio"
-          id="small"
-          name="colors"
-          value="small"
-        />
-        <label
-          className={`${sizes === "small" ? "button-selected" : "button"}`}
-          for="small"
-        >
-          small
-        </label>
-        <input
-          onChange={(e) => setSizes(e.target.value)}
-          className="hidden_radio"
-          type="radio"
-          id="medium"
-          name="colors"
-          value="medium"
-        />
-        <label
-          className={`${sizes === "medium" ? "button-selected" : "button"}`}
-          for="medium"
-        >
-          medium
-        </label>
-        <input
-          onChange={(e) => setSizes(e.target.value)}
-          className="hidden_radio"
-          type="radio"
-          id="large"
-          name="colors"
-          value="large"
-        />
-        <label
-          className={`${sizes === "large" ? "button-selected" : "button"}`}
-          for="large"
-        >
-          large
-        </label>
-        <input
-          onChange={(e) => setSizes(e.target.value)}
-          className="hidden_radio"
-          type="radio"
-          id="xlarge"
-          name="colors"
-          value="xlarge"
-        />
-        <label
-          className={`${sizes === "xlarge" ? "button-selected" : "button"}`}
-          for="xlarge"
-        >
-          xlarge
-        </label>
-      </div>
-
-				<h2 className="color-choice">COLOR</h2>
-				
-				<div className="colorbutton-style">
-					<input
-          onChange={(e) => setColors(e.target.value)}
-          className="hidden_radio"
-          type="radio"
-          id="color1"
-          name="colors"
-          value="color 1"
-        />
-        <label
-          className={`${colors === "color 1" ? "button-selected" : "button"}`}
-          for="color1"
-        >
-          color 1
-        </label>
-        <input
-          onChange={(e) => setColors(e.target.value)}
-          className="hidden_radio"
-          type="radio"
-          id="color2"
-          name="colors"
-          value="color 2"
-        />
-        <label
-          className={`${colors === "color 2" ? "button-selected" : "button"}`}
-          for="color2"
-        >
-          color 2
-        </label>
-      </div>
-    
-				<h2 className="price-choice">PRICE</h2>
+        <div className="colorbutton-style">
+          <input
+            onChange={(e) => this.setState({ colors: e.target.value })}
+            className="hidden_radio"
+            type="radio"
+            id="color1"
+            name="colors"
+            value="color 1"
+          />
+          <label
+            className={`${
+              this.state.colors === "color 1" ? "button-selected" : "button"
+            }`}
+            for="color1"
+          >
+            color 1
+          </label>
+          <input
+            onChange={(e) => this.setState({ colors: e.target.value })}
+            className="hidden_radio"
+            type="radio"
+            id="color2"
+            name="colors"
+            value="color 2"
+          />
+          <label
+            className={`${
+              this.state.colors === "color 2" ? "button-selected" : "button"
+            }`}
+            for="color2"
+          >
+            color 2
+          </label>
+        </div>
+        <h2 className="price-choice">PRICE</h2>
 				<p className="product-price">{price}</p>
 
 				<input className="cta-button" type="button" value="ADD TO CART" name="ADD" />
 				 </div>{" "}
 			<p className="product-desc">{productDesc}</p>
 		</div>
+    </div>
 	);
 				}
 			}
